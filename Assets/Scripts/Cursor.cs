1b9f60a0423f5ef7788b313d69c58b274a8e039d
knh190 <knh190@hotmail.com>
2022-03-20 12:45:08 -0400

    create tiles

--- Assets/Scripts/Cursor.cs
+++ Assets/Scripts/Cursor.cs
@@ -1,9 +1,30 @@
 using UnityEngine;
 
+[RequireComponent(typeof(TileMap))]
 public class Cursor : MonoBehaviour
 {
     // @todo get input from keyboard
     // @todo move cursor
     // @todo activate and stop sound tiles
     // @todo display player sprite
+
+    private TileMap map;
+
+    void Awake()
+    {
+        map = GetComponent<TileMap>();
+    }
+
+    void Update()
+    {
+        //
+    }
+
+    void GetInput()
+    {
+        if (Input.GetKeyDown(KeyCode.A)) {}
+        if (Input.GetKeyDown(KeyCode.W)) {}
+        if (Input.GetKeyDown(KeyCode.S)) {}
+        if (Input.GetKeyDown(KeyCode.D)) {}
+    }
 }
--- Assets/Scripts/TileMap.cs
+++ Assets/Scripts/TileMap.cs
@@ -4,4 +4,49 @@ public class TileMap: MonoBehaviour
 {
     // @todo align tiles
     // @todo track player pos
+
+    private TileSound[] tiles;
+
+    public TileSound[] prefabs;
+
+    [Min(2)]
+    public int width;
+    [Min(2)]
+    public int height;
+
+    public void DestroyTiles()
+    {
+        if (tiles != null)
+        {
+            foreach (TileSound tile in tiles)
+            {
+                Destroy(tile.gameObject);
+            }
+            tiles = new TileSound[0];
+
+            Debug.Log("Tiles cleared.");
+        }
+    }
+
+    public void CreateTiles()
+    {
+        DestroyTiles();
+
+        Debug.Log("Creating tiles: " + width + " x " + height);
+
+        tiles = new TileSound[width * height];
+
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j ++)
+            {
+                int idx = Mathf.FloorToInt(Random.Range(0, prefabs.Length));
+                TileSound prefab = prefabs[idx];
+                Vector3 pos = transform.position + new Vector3(i, 0, j);
+
+                TileSound tile = Instantiate(prefab, pos, Quaternion.identity);
+                tiles[j * width + i] = tile;
+            }
+        }
+    }
 }
