b0b23d1686770b5fe4b593bcfc246599fef131ab
MIDIManNoah <59802822+MIDIManNoah@users.noreply.github.com>
2022-03-24 20:12:29 -0400

    Add the red tile to the project (#1)
    * Add red tile sprites
    * Made the red tiles animate
    * Updated TileSound.cs to support animations
    
    A tile's animation will only play when the player is on the tile

--- Assets/Scripts/TileSound.cs
+++ Assets/Scripts/TileSound.cs
@@ -6,23 +6,40 @@ using UnityEngine;
 public class TileSound : MonoBehaviour
 {
     private AudioSource audio;
+    private Animator animator;
     public string colorName;
 
     void Awake() 
     {
         audio = GetComponent<AudioSource>();
+        animator = GetComponent<Animator>();
+        if (animator != null)
+        {
+            animator.enabled = true;
+            animator.speed = 0;
+        }
         audio.enabled = false;
     }
 
     public void Play()
     {
         audio.enabled = true;
+        if (animator != null)
+        {
+            animator.Play(0, -1, 0);
+            animator.speed = 1;
+        }
         audio.Play();
     }
 
     public void Stop()
     {
         audio.Stop();
+        if (animator != null)
+        {
+            animator.Play(0, -1, 0);
+            animator.speed = 0;
+        }
         audio.enabled = false;
     }
 }
